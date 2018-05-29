c0d1c05aa4aebe11a919a9fc51697c2bf1f791e4
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-05-29 17:05:14 +1000

    Add ut mananger for seL4 10.0.0
    This commit adds an untyped manager which manages untyped objects of 4k and
    less.

---
+++ apps/sos/src/ut.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+#include "ut.h"
+#include "bootstrap.h"
+
+#include <cspace/cspace.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define SIZE_BITS_TO_INDEX(x) (x - seL4_EndpointBits)
+
+/* this global variable tracks our ut table */
+static ut_table_t table;
+
+static void push(ut_t **head, ut_t *new)
+{
+    new->next = *head;
+    *head = new;
+}
+
+static ut_t *pop(ut_t **head)
+{
+    ut_t *popped = *head;
+    *head = (*head)->next;
+    return popped;
+}
+
+static inline seL4_Word ut_to_paddr(ut_t *ut)
+{
+    return (ut - table.untypeds) * PAGE_SIZE_4K + table.first_paddr;
+}
+
+static inline ut_t *paddr_to_ut(seL4_Word paddr)
+{
+    return &table.untypeds[(paddr - table.first_paddr) / PAGE_SIZE_4K];
+}
+
+size_t ut_pages_for_region(ut_region_t region)
+{
+    /* work out the size of the ut table for a region of memory */
+    return BYTES_TO_4K_PAGES((region.end - region.start) / PAGE_SIZE_4K * sizeof(ut_t));
+}
+
+void ut_init(void *memory, ut_region_t region)
+{
+    /* the memory is already 0'd, as its just been allocated from raw untyped */
+    table.untypeds = memory;
+    memset(table.free_untypeds, 0, ARRAY_SIZE(table.free_untypeds));
+    table.first_paddr = region.start;
+}
+
+void ut_add_untyped_range(seL4_Word paddr, seL4_CPtr cap, size_t n, bool device)
+{
+    ut_t **list = &table.free_untypeds[SIZE_BITS_TO_INDEX(seL4_PageBits)];
+    for (size_t i = 0; i < n; i++) {
+        ut_t *node = paddr_to_ut(paddr + (i * PAGE_SIZE_4K));
+        node->cap = cap;
+        cap++;
+        if (!device) {
+            push(list, node);
+        }
+    }
+}
+
+ut_t *ut_alloc_4k_untyped(uintptr_t *paddr)
+{
+    ut_t **list = &table.free_untypeds[SIZE_BITS_TO_INDEX(seL4_PageBits)];
+    if (*list == NULL) {
+        ZF_LOGE("out of memory");
+        return NULL;
+    }
+
+    ut_t *n = pop(list);
+    if (paddr) {
+        *paddr = ut_to_paddr(n);
+    }
+    ZF_LOGD("Allocated %lx, cap %lx", ut_to_paddr(n), n->cap);
+    return n;
+}
+
+/* ensure there are at least two spare free structures so we can split an untyped */
+static bool ensure_new_structures(cspace_t *cspace)
+{
+    if (table.free_structures == NULL || table.free_structures->next == NULL) {
+        /* we need to allocate more spare ut objects */
+        ut_t *frame = ut_alloc_4k_untyped(NULL);
+        if (frame == NULL) {
+            /* No luck */
+            ZF_LOGE("No 4K untypeds");
+            return false;
+        }
+
+        /* allocate a slot to retype the frame into */
+        seL4_CPtr cptr = cspace_alloc_slot(cspace);
+        if (cptr == seL4_CapNull) {
+            /* cspace is full */
+            ZF_LOGE("Cspace full");
+            return false;
+        }
+
+        /* retype */
+        seL4_Error err = cspace_untyped_retype(cspace, frame->cap, cptr, seL4_ARM_SmallPageObject, seL4_PageBits);
+        if (err) {
+            ZF_LOGE("Retype failed");
+            return false;
+        }
+
+        /* map */
+        ut_t *new_uts = bootstrap_map_frame(cspace, cptr);
+        if (new_uts == NULL) {
+            ZF_LOGE("Failed to map frame");
+            return false;
+        }
+
+        /* now add all the new uts structures to the free list */
+        for (int i = 0; i < PAGE_SIZE_4K / sizeof(ut_t); i++) {
+            push(&table.free_structures, &new_uts[i]);
+        }
+    }
+    return true;
+
+}
+
+ut_t *ut_alloc(size_t size_bits, cspace_t *cspace)
+{
+    /* check we can handle the size */
+    if (size_bits > seL4_PageBits) {
+        ZF_LOGE("UT table can only allocate untypeds <= 4K in size");
+        return NULL;
+    }
+
+    if (size_bits < seL4_EndpointBits) {
+        ZF_LOGE("UT table cannot alloc untyped < %zu in size\n", (size_t) seL4_EndpointBits);
+        return NULL;
+    }
+
+    if (size_bits == seL4_PageBits) {
+        return ut_alloc_4k_untyped(NULL);
+    }
+
+    ut_t **list = &table.free_untypeds[SIZE_BITS_TO_INDEX(size_bits)];
+    if (*list == NULL) {
+        /* need to retype a bigger object into the size requested */
+        ut_t *larger = ut_alloc(size_bits + 1, cspace);
+        if (larger == NULL) {
+            return NULL;
+        }
+
+        /* check we have enough memory to account for our new uts */
+        if (!ensure_new_structures(cspace)) {
+            ut_free(larger, seL4_PageBits);
+            return NULL;
+        }
+
+        /* now ask the cspace to retype for us */
+        ut_t *new1 = pop(&table.free_structures);
+        new1->cap = cspace_alloc_slot(cspace);
+        if (new1->cap == seL4_CapNull) {
+            ut_free(larger, seL4_PageBits);
+            return NULL;
+        }
+        ut_t *new2 = pop(&table.free_structures);
+        new2->cap = cspace_alloc_slot(cspace);
+        if (new2->cap == seL4_CapNull) {
+            cspace_free_slot(cspace, new1->cap);
+            ut_free(larger, seL4_PageBits);
+            return NULL;
+        }
+
+        seL4_Error err = cspace_untyped_retype(cspace, larger->cap, new1->cap, seL4_UntypedObject, size_bits);
+        if (err) {
+            cspace_free_slot(cspace, new1->cap);
+            cspace_free_slot(cspace, new2->cap);
+            push(&table.free_structures, new1);
+            ut_free(larger, seL4_PageBits);
+            return NULL;
+        }
+
+        err = cspace_untyped_retype(cspace, larger->cap, new2->cap, seL4_UntypedObject, size_bits);
+        if (err) {
+            cspace_delete(cspace, new1->cap);
+            cspace_free_slot(cspace, new1->cap);
+            cspace_free_slot(cspace, new2->cap);
+            push(&table.free_structures, new1);
+            push(&table.free_structures, new2);
+            ut_free(larger, seL4_PageBits);
+            return NULL;
+        }
+
+        push(list, new1);
+        push(list, new2);
+        /* finally, we now know there are untyped objects for the requested size */
+    }
+
+    return pop(list);
+}
+
+void ut_free(ut_t *node, size_t size_bits)
+{
+    if (size_bits < seL4_EndpointBits || size_bits > seL4_PageBits) {
+        ZF_LOGE("Invalid size bits %zu", size_bits);
+        return;
+    }
+
+    ut_t **list = &table.free_untypeds[SIZE_BITS_TO_INDEX(size_bits)];
+    push(list, node);
+}
+
+ut_t *ut_alloc_4k_device(uintptr_t paddr)
+{
+    return paddr_to_ut(paddr);
+}
---
+++ apps/sos/src/ut.h
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+#pragma once
+
+/*
+ * This is an untyped object allocator which tracks objects of 4k and less in size.
+ *
+ * You may extend this allocator to track further details about 4k frames.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <sel4/sel4.h>
+#include <utils/util.h>
+#include <cspace/cspace.h>
+
+typedef struct {
+    /* the lowest valid address in the region */
+    seL4_Word start;
+    /* the highest valid address in the region +1 */
+    seL4_Word end;
+} ut_region_t;
+
+/* a specific untyped */
+typedef struct ut ut_t;
+struct ut {
+    seL4_CPtr cap;
+    ut_t *next;
+};
+
+/* list of valid object sizes we can allocate */
+#define N_UNTYPED_LISTS (seL4_PageBits - seL4_EndpointBits + 1)
+
+/* Untyped memory table */
+typedef struct {
+    /* first paddr in the table of 4K untypeds */
+    seL4_Word first_paddr;
+    /* region of 4K untypeds - one for each untyped . */
+    ut_t *untypeds;
+    /* list of free untypeds, one list per object size. The untyped list at seL4_PageBits
+     * is a free list in the untypeds region. The rest are sublists, with bookkeeping data allocated
+     * from an untyped */
+    ut_t *free_untypeds[N_UNTYPED_LISTS];
+    /* list of unused nodes which can be used to populate untyped lists
+     * of untyped objects < 4K in size, where the bookkeping data is allocated on demand from the 4k
+     * untypeds free list */
+    ut_t *free_structures;
+} ut_table_t;
+
+/* return the size (in 4K pages) of the table required to cover a specific region */
+size_t ut_pages_for_region(ut_region_t region);
+
+/**
+ * Initialise the ut allocator (empty). ut_add_untyped_range must be called to populate the
+ * allocator with 4k untyped objects before it can allocate.
+ *
+ * @param memory pointer to mapped memory sufficient to cover the bookkeeping for the memory provided
+ *               in the region parameter. More specifically, memory must be
+ *               >= ut_pages_for_region(region) * sizeof(ut_t).
+ *               We assume that the memory provided is already zeroed.
+ * @param region memory region the ut should cover.
+ *
+ * */
+void ut_init(void *memory, ut_region_t region);
+
+/* Add a contiguous (in memory and capabilities) set of 4k untypeds to the table. The paddr
+ * must be in the range provided to ut_init with the region parameter.
+ *
+ * @param paddr  the physical address that the untypeds start at
+ * @param cap    the start of the range of cptrs where the untypeds are
+ * @param n      the number of untypeds in the contiguous region of caps and paddrs.
+ * @param device is this a device untyped or not.
+ */
+void ut_add_untyped_range(seL4_Word paddr, seL4_CPtr cap, size_t n, bool device);
+
+/**
+ * Allocate an untyped object of 4K in size. This operation will *never* result in a
+ * cspace allocation as all 4K objects are pre-allocated.
+ *
+ * @param paddr[out]  return the physical address of the untyped memory here. NULL
+ *                    if no value should be returned.
+ * @return            a pointer to the 4K table entry that represents the 4K
+ *                    untyped returned, containing the capability which can then be
+ *                    passed to seL4_Untyped_Retype.
+ *                    NULL if no memory is available.
+ */
+ut_t *ut_alloc_4k_untyped(uintptr_t *paddr);
+
+/**
+ * Allocate an untyped of a specific size < seL4_PageBits.
+ *
+ * This operation may result in cspace allocations, as we may need to retype untyped
+ * objects of bigger sizes until we get the size we need, if free untyped objects of
+ * the correct size are unavailable. For this reason this function must be passed a
+ * cspace, which must not call back into this function to avoid infinite recursion. The
+ * cspace *can* call into ut_alloc_alloc_4k_untyped.
+ *
+ * @param size_bits    the amount of contiguous and aligned memory to reserve (2^size_bits)
+ * @param cspace_alloc a cspace which can be used to allocate slots.
+ * @return             A pointer which can be used to free the allocation.
+ *                     NULL if no memory is available.
+ */
+
+ut_t *ut_alloc(size_t size_bits, cspace_t *cspace_alloc);
+
+/**
+ * Mark an untyped object as free. This will make the object available for reallocation.
+ * This allocator does not remerge untyped objects: if you allocate your entire
+ * set of memory as objects < seL4_PageBits, you will not be able to allocate any
+ * bigger objects. It is possible to merge untypeds, but this is not implemented to
+ * reduce complexity.
+ *
+ * Allocations made with ut_alloc_frame and ut_alloc can both be freed with this function.
+ *
+ * @param ut        the ut pointer returned by ut_alloc or ut_alloc_4k_untyped
+ * @param size_bits the size to the memory to free, that was used for the allocation.
+ */
+void ut_free(ut_t *ut, size_t size_bits);
+
+/**
+ * Allocate 4K of device untyped at a specific physical address. These are only pulled
+ * from the untyped which have been passed to ut_add_untyped_region with device=true.
+ *
+ * @param paddr the physical address to allocate.
+ * @return      cptr to the allocated untyped. seL4_CapNull if the paddr is not available.
+ */
+ut_t *ut_alloc_4k_device(uintptr_t paddr);
