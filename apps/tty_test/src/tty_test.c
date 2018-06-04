d314b4f2a2efac1ec9b17ef1b7785fdbd33144e6
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-06-04 14:07:56 +1000

    tty_test: remove call to SetTag
    it's deprecated

--- apps/tty_test/src/tty_test.c
+++ apps/tty_test/src/tty_test.c
@@ -34,7 +34,6 @@ static void
 thread_block(void)
 {
     seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
-    seL4_SetTag(tag);
     seL4_SetMR(0, 1);
     seL4_Call(SYSCALL_ENDPOINT_SLOT, tag);
 }
