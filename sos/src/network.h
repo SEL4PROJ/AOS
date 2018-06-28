24bdb0470d92f1269afe5b42d1cde1552d7341f0
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-06-28 16:25:01 +1000

    sos: remove bulk of network.c

--- sos/src/network.h
+++ sos/src/network.h
@@ -7,33 +7,21 @@
  *
  * @TAG(NICTA_BSD)
  */
-
-#ifndef NETWORK_H
-#define NETWORK_H
+#pragma once
 
 #include <sel4/types.h>
-#include <nfs/nfs.h>
-
-extern fhandle_t mnt_point;
+#include <cspace/cspace.h>
 
 /**
  * Initialises the network stack
- * @param[in] interrupt_ep The asynchronous endpoint that the 
- *                         driver should use for registering IRQs
+ *
+ * @param cspace         for creating slots for mappings
+ * @param interrupt_ntfn The notification object that the driver should use for registering IRQs
+ *
  */
-extern void network_init(seL4_CPtr interrupt_ep);
+void network_init(cspace_t *cspace, seL4_CPtr interrupt_ntfn);
 
 /**
- * Initialises DMA memory for the network driver
- * @param[in] paddr    The base physical address of the memory to use for DMA
- * @param[in] sizebits The size (1 << sizebits bytes) of the memory provided.
- * @return             0 on success
+ * Tell the network driver to handle any pending events
  */
-extern int dma_init(seL4_Word paddr, int sizebits);
-
-/**
- * Allows the network driver to handle any pending events
- */
-extern void network_irq(void);
-
-#endif
+void network_irq(void);
