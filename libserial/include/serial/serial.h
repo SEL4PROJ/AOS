770ef81f584a698f3253d36243362fdb5b409087
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-07-05 16:25:23 +1000

    trivial: style

--- libserial/include/serial/serial.h
+++ libserial/include/serial/serial.h
@@ -8,7 +8,6 @@
  * @TAG(NICTA_BSD)
  */
 
-extern struct serial *serial_init(void);
-extern int serial_send(struct serial *serial, char *data, int len);
-extern int serial_register_handler(struct serial *serial, 
-			void (*handler) (struct serial *serial, char c));
+struct serial *serial_init(void);
+int serial_send(struct serial *serial, char *data, int len);
+int serial_register_handler(struct serial *serial, void (*handler) (struct serial *serial, char c));
