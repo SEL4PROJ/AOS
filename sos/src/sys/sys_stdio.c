ebccc301b2e5573ee6d9addd5ddf5e4e522d77e5
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-07-05 15:10:27 +1000

    Revert "sys_stdio: work around pico/nfs incompatibilities"
    This reverts commit 41dd710d6ba360a8236e8423789c95be91e0e052, as
    manipulating the error codes broke libnfs in other exciting ways.

--- sos/src/sys/sys_stdio.c
+++ sos/src/sys/sys_stdio.c
@@ -198,15 +198,8 @@ long sys_connect(va_list ap)
     socklen_t socklen = va_arg(ap, socklen_t);
 
     if (sd >= PICO_FD_START) {
-        if (pico_connect(sd - PICO_FD_START, _saddr, socklen) == 0) {
-            return 0;
-        } else if (errno == EAGAIN) {
-            /* picoTCP reports EAGAIN instead of EINPROGRESS as an async connection return code.
-             * As a workaround, treat EAGAIN the same as EINPROGRESS. */
-            return -EINPROGRESS;
-        } else {
-            return -errno;
-        }
+        int err = pico_connect(sd - PICO_FD_START, _saddr, socklen);
+        return err == 0 ? err : -errno;
     }
     return -EINVAL;
 }
@@ -350,14 +343,7 @@ long sys_getsockopt(va_list ap)
     socklen_t *optlen = va_arg(ap, socklen_t *);
 
     if (sockfd >= PICO_FD_START) {
-        int err = pico_getsockopt(sockfd - PICO_FD_START, level, optname, optval, optlen);
-        if (err == -1) {
-            /* picoTCP reports EAGAIN even after a socket is correctly connected,
-             * so we ignore EAGAIN error codes here. */
-            return errno == EAGAIN ? 0 : -errno;
-        } else {
-            return 0;
-        }
+        return pico_getsockopt(sockfd - PICO_FD_START, level, optname, optval, optlen);
     }
     return -EINVAL;
 }
