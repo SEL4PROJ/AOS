55da7ef1722f0dc2bb46d870de398e85c88678e2
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2018-07-11 12:55:49 +1000

    libethernet: implement library interface

--- libethernet/src/ethernet.c
+++ libethernet/src/ethernet.c
@@ -9,5 +9,83 @@
  *
  * @TAG(DATA61_GPL)
  */
+#include <errno.h>
+#include <assert.h>
+
+#include "uboot/common.h"
+#include "uboot/net.h"
+#include "uboot/miiphy.h"
+#include "io.h"
+#include "uboot/netdev.h"
+#include "unimplemented.h"
 
 #include <ethernet/ethernet.h>
+#include <utils/zf_log.h>
+
+static struct eth_device uboot_eth_dev;
+
+ethif_dma_ops_t dma_ops;
+
+ethif_err_t ethif_send(uint8_t *buf, uint32_t len)
+{
+    assert(buf);
+    return (uboot_eth_dev.send(&uboot_eth_dev, buf, len) == 0) ? ETHIF_NOERROR : ETHIF_ERROR;
+}
+
+ethif_err_t ethif_recv(int *len)
+{
+    assert(len);
+    int result = uboot_eth_dev.recv(&uboot_eth_dev);
+    if(result >= 0) {
+        *len = result;
+        return ETHIF_NOERROR;
+    }
+    return ETHIF_ERROR;
+}
+
+ethif_dma_ops_t *uboot_get_dma_ops()
+{
+    return &dma_ops;
+}
+
+ethif_err_t ethif_init(ethif_dma_ops_t *ops, uint64_t base_addr, const uint8_t mac[6])
+{
+    assert(ops);
+
+    ZF_LOGI("Initialising ethernet interface...");
+
+    /* Save a copy of the DMA ops for use by the driver */
+    memcpy(&dma_ops, ops, sizeof(ethif_dma_ops_t));
+
+    uboot_timer_init();
+
+    /* Initialise the MII abstraction layer */
+    miiphy_init();
+
+    /* Initialise the actual Realtek PHY */
+    phy_init();
+
+    /* Populate the eth_dev functions, also does some more PHY init */
+    int ret = designware_initialize(base_addr, 0, &uboot_eth_dev);
+
+    if (ret != 0) {
+        ZF_LOGE("Failed: designware_initialize.");
+        return ETHIF_ERROR;
+    }
+
+    /* NOTE: This line determines what our MAC address will be, it is
+     * internally programmed into the device during the next init step*/
+    memcpy(uboot_eth_dev.enetaddr, mac, 6);
+
+    /* Bring up the interface - the last initialisation step */
+    ret = uboot_eth_dev.init(&uboot_eth_dev);
+
+    if (ret != 0) {
+        ZF_LOGE("Failed: uboot_eth_dev.init().");
+        return ETHIF_ERROR;
+    }
+
+    ZF_LOGI("interface UP");
+
+    return ETHIF_NOERROR;
+}
