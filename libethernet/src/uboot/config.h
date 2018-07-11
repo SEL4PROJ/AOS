89b10994fd91121bd5592c10be0728ef255fef6f
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2018-07-11 12:01:49 +1000

    libethernet: add configuration defines for C2

--- libethernet/src/uboot/config.h
+++ libethernet/src/uboot/config.h
@@ -2,4 +2,14 @@
  * @TAG(OTHER_GPL)
  */
 
-#include "zynq_zc70x.h"
+#pragma once
+
+#define CONFIG_PHY_REALTEK 1
+#define CONFIG_SYS_HZ 1000 /* Current timer header uses milliseconds for get_timer */
+#define CONFIG_ARM 1
+#define CONFIG_ARCH_MESON 1
+#define CONFIG_MESON_GXBB 1
+#define CONFIG_TARGET_ODROID_C2 1
+#define CONFIG_NET_RANDOM_ETHADDR 1
+#define CONFIG_ETH_DESIGNWARE 1
+#define CONFIG_PHYLIB 1
