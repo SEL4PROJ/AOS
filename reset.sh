6a5e85b343c9d0f4ff8b83cfe89bb2dcb4eef839
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-07-16 16:31:39 +1000

    add reset.sh
    This script copies the image to the tftpboot dir and resets the board

---
+++ reset.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright 2018, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the GNU General Public License version 2. Note that NO WARRANTY is provided.
+# See "LICENSE_GPLv2.txt" for details.
+#
+# @TAG(DATA61_GPL)
+#
+set -e
+
+TFTPROOT=/var/tftpboot/${USER}
+SERIAL_PORT=/dev/ttyUSB0
+echo "cp ${PWD}/images/sos-image-arm-odroidc2 ${TFTPROOT}"
+cp ${PWD}/images/sos-image-arm-odroidc2 ${TFTPROOT}
+echo "echo \"reset\" >> ${SERIAL_PORT}"
+echo "reset" >> ${SERIAL_PORT}
