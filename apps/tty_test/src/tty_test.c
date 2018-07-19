3dafa66b6827c61e6686d00179052e229e8a279a
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-07-19 10:15:19 +1000

    tty_test: add more comments to help with m0

--- apps/tty_test/src/tty_test.c
+++ apps/tty_test/src/tty_test.c
@@ -34,9 +34,15 @@
 static void
 thread_block(void)
 {
+    /* construct some info about the IPC message tty_test will send
+     * to sos -- it's 1 word long */
     seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
+    /* Set the first word in the message to 0 */
     seL4_SetMR(0, 1);
+    /* Now send the ipc -- call will send the ipc, then block until a reply
+     * message is received */
     seL4_Call(SYSCALL_ENDPOINT_SLOT, tag);
+    /* Currently SOS does not reply -- so we never come back here */
 }
 
 int main(void)
