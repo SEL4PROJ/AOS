329ca598390a17c0ae2ef28718cbf0bd83b2590f
Curtis Millar <curtis.millar@data61.csiro.au>
2020-01-31 11:32:08 +1100

    Make all licenses GPL
    All code in this repository must by GPL compatible. No BSD cluases here.

--- aos-make-project.sh
+++ aos-make-project.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-# Copyright 2019, Data61
+# Copyright 2020, Data61
 # Commonwealth Scientific and Industrial Research Organisation (CSIRO)
 # ABN 41 687 119 230.
 #
 # This software may be distributed and modified according to the terms of
-# the BSD 2-Clause license. Note that NO WARRANTY is provided.
-# See "LICENSE_BSD2.txt" for details.
+# the GNU General Public License version 2. Note that NO WARRANTY is provided.
+# See "LICENSE_GPLv2.txt" for details.
 #
-# @TAG(DATA61_BSD)
+# @TAG(DATA61_GPL)
 #
-#
+
 # Script to extract the aos sources from a repo managed repository into
 # a single dir, and maintaining the correct symbolic links. If no
 # directory exists a git repository is initialised. All updates to the
