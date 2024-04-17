SEL4PROJ/AOS

commit 57d4b41052e9d23ebf6ca8c40b27c473258c2cdc

sos: Fix bugs in cspace detected by tests

libsel4cspace/include/cspace/bitfield.h
@@ -46,9 +46,14 @@ static inline unsigned long bf_first_free(size_t words, unsigned long bits[words
     for (; i < words && bits[i] == ULONG_MAX; i++);
 
     unsigned long bit = i * WORD_BITS;
+
+    /* we want to find the first 0 bit, do this by inverting the value */
+    unsigned long val = ~bits[i];
     /* it's illegal to call CLZL on 0, so check first */
-    if (i < words && bits[i] > 0) {
-        bit += (WORD_BITS - CLZL(bits[i]));
+    assert(val != 0);
+
+    if (i < words) {
+        bit += (CTZL(val));
     }
     return bit;
 }

libsel4cspace/src/cspace.c
@@ -74,7 +74,7 @@ static void refill_watermark(cspace_t *cspace, seL4_Word *used)
     for (int i = 0; i < WATERMARK_SLOTS; i++) {
         if (*used & BIT(i)) {
             cspace->watermark[i] = cspace_alloc_slot(cspace);
-            ZF_LOGW_IF(cspace->watermark[i] != seL4_CapNull, "Cspace full in watermark function");
+            ZF_LOGW_IF(cspace->watermark[i] == seL4_CapNull, "Cspace full in watermark function");
             break;
         }
     }
@@ -124,7 +124,7 @@ static bool ensure_levels(cspace_t *cspace, seL4_CPtr cptr, int n_slots, seL4_Wo
 
     assert(cspace->bot_lvl_nodes[node] != NULL);
     seL4_Word cnode = CNODE_INDEX(cptr);
-    if (cspace->bot_lvl_nodes[node]->n_cnodes < cnode) {
+    if (cspace->bot_lvl_nodes[node]->n_cnodes <= cnode) {
         /* need to allocate a new cnode */
         seL4_CPtr ut_cptr;
         cspace->bot_lvl_nodes[node]->cnodes[cnode].untyped = alloc_4k_untyped(&cspace->alloc, &ut_cptr);
@@ -321,8 +321,8 @@ seL4_CPtr cspace_alloc_slot(cspace_t *cspace)
         cptr = top_index << CNODE_SLOT_BITS(CNODE_SIZE_BITS);
 
         /* ensure the bottom level cnode is present */
-        if (cspace->n_bot_lvl_nodes < NODE_INDEX(cptr) ||
-                cspace->bot_lvl_nodes[NODE_INDEX(cptr)]->n_cnodes < CNODE_INDEX(cptr)) {
+        if (cspace->n_bot_lvl_nodes <= NODE_INDEX(cptr) ||
+            cspace->bot_lvl_nodes[NODE_INDEX(cptr)]->n_cnodes <= CNODE_INDEX(cptr)) {
             if (!ensure_levels(cspace, cptr, MAPPING_SLOTS, &used)) {
                 return seL4_CapNull;
             }
@@ -331,13 +331,13 @@ seL4_CPtr cspace_alloc_slot(cspace_t *cspace)
         /* now allocate a bottom level index */
         bot_lvl_t *bot_lvl = &cspace->bot_lvl_nodes[NODE_INDEX(cptr)]->cnodes[CNODE_INDEX(cptr)];
         seL4_Word bot_index = bf_first_free(BITFIELD_SIZE(CNODE_SIZE_BITS), bot_lvl->bf);
-        if (bot_index == CNODE_SLOTS(CNODE_SIZE_BITS)) {
+        if (bot_index >= CNODE_SLOTS(CNODE_SIZE_BITS)) {
             ZF_LOGE("Cspace is full!\n");
             return seL4_CapNull;
         }
 
         bf_set_bit(bot_lvl->bf, bot_index);
-        if (bot_index == CNODE_SLOTS(CNODE_SIZE_BITS) - 1) {
+        if (bot_index >= CNODE_SLOTS(CNODE_SIZE_BITS) - 1) {
             /* we just allocated the last slot -> mark the top level as full */
             bf_set_bit(cspace->top_bf, top_index);
         }

commit fa406e33748db5583cec4ca29f8fb09bb1a4dace

cspace: Fix bitfield bug introduced by 57d4b41
In the course of fixing other bugs, 57d4b41 moved the read of the bits
array outside the bounds checking of the index. The bug can manifest as
a failed assertion if the value on the stack preceding the bits array is
ULONG_MAX. In any case, it's probably best not to touch data outside the
bounds of the provided array.

libsel4cspace/include/cspace/bitfield.h
@@ -47,12 +47,12 @@ static inline unsigned long bf_first_free(size_t words, unsigned long bits[words
 
     unsigned long bit = i * WORD_BITS;
 
-    /* we want to find the first 0 bit, do this by inverting the value */
-    unsigned long val = ~bits[i];
-    /* it's illegal to call CLZL on 0, so check first */
-    assert(val != 0);
-
     if (i < words) {
+        /* we want to find the first 0 bit, do this by inverting the value */
+        unsigned long val = ~bits[i];
+        /* it's illegal to call CLZL on 0, so check first */
+        assert(val != 0);
+
         bit += (CTZL(val));
     }
     return bit;